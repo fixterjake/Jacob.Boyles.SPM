13213cf56d4ef966278c7751ee7412919a36c310
Jacob Boyles <fixterjake@gmail.com>
2020-09-15 18:53:16 -0400

    Renamed folders, project, and solution to better match project

--- SPM.Web/Startup.cs
+++ SPM.Web/Startup.cs
@@ -32,7 +32,7 @@ namespace Jacob.Boyles.Management
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
