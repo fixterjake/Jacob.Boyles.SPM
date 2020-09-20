c1fb49f88cf6705d4f263f15fd49e88e779ef6b1
Jacob Boyles <fixterjake@gmail.com>
2020-09-20 17:31:35 -0400

    Remove about page

--- SPM.Web/Controllers/HomeController.cs
+++ SPM.Web/Controllers/HomeController.cs
@@ -24,11 +24,6 @@ namespace SPM.Web.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            return View();
-        }
-
         public IActionResult Privacy()
         {
             return View();
